366621a0bcf4da0e6d2e376dfd992554d95e6b2a
Ivan Gonzalez <108gonzalezdelgado@gmail.com>
2025-11-11 22:40:35 -0600

    Primer subida de mi portafolio

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', 
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+  ],
+  theme: {
+  },
+  plugins: [],
+};
